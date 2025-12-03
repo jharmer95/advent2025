1c03f7db76f8a14a2a30b00bbdbb67bdf8e0404b
Jackson <jharmer95@gmail.com>
2025-12-03 14:55:05 -0500

    Implement day 1

--- tests/final_tests.rs
+++ tests/final_tests.rs
@@ -2,3 +2,21 @@
 
 use advent2025::inputs::get_input;
 use advent2025::solutions::*;
+
+#[test]
+fn day01_part1() {
+    let input = get_input("inputs/day_01.txt").expect("Could not parse path!");
+    let mut solution = Day01::default();
+    solution.parse_input(&input);
+    let output = solution.part1();
+    assert_eq!(output, "1071");
+}
+
+#[test]
+fn day01_part2() {
+    let input = get_input("inputs/day_01.txt").expect("Could not parse path!");
+    let mut solution = Day01::default();
+    solution.parse_input(&input);
+    let output = solution.part2();
+    assert_eq!(output, "6700");
+}
